4631feade2435188d5087018b2a6f581d6f64158
kirill <frolovk1307@gmail.com>
2019-06-25 17:49:55 +0300

    helper function non exists condition

--- app/helpers.php
+++ app/helpers.php
@@ -6,23 +6,25 @@
  * Time: 21:25
  */
 
-/**
- * Returns cryptographic random or pseudorandom integer
- * @param int|null $min
- * @param int|null $max
- * @return int
- */
-function crypto_rand_int(int $min = null, int $max = null): int
-{
-    $min = is_null($min) ? -mt_getrandmax() : $min;
-    $max = is_null($max) ? mt_getrandmax() : $max;
+if (!function_exists('crypto_rand_int')) {
+    /**
+     * Returns cryptographic random or pseudorandom integer
+     * @param int|null $min
+     * @param int|null $max
+     * @return int
+     */
+    function crypto_rand_int(int $min = null, int $max = null): int
+    {
+        $min = is_null($min) ? -mt_getrandmax() : $min;
+        $max = is_null($max) ? mt_getrandmax() : $max;
 
-    try {
-        $value = random_int($min, $max);
-    } catch (\Exception $exception) {
-        \Log::warning('Random value isn\'t cryptographic random');
-        $value = mt_rand($min, $max);
+        try {
+            $value = random_int($min, $max);
+        } catch (\Exception $exception) {
+            \Log::warning('Random value isn\'t cryptographic random');
+            $value = mt_rand($min, $max);
+        }
+
+        return $value;
     }
-
-    return $value;
 }
